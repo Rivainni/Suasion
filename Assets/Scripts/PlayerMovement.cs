502b34a74206f2a778f1fd625ee09defd378c64b
Anton Ralph F. Valenzuela <antonralphvalenzuela68@gmail.com>
2022-12-07 22:34:15 +0800

    8 way movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,21 +19,26 @@ public class PlayerMovement : MonoBehaviour
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
 
-            animator.SetFloat("Horizontal", movement.x);
-            animator.SetFloat("Vertical", movement.y);
+            animator.SetInteger("Horizontal", Mathf.RoundToInt(movement.x));
+            animator.SetInteger("Vertical", Mathf.RoundToInt(movement.y));
             animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            Debug.Log("X: " + movement.x + ", Y :" + movement.y);
         }
         else
         {
-            animator.SetFloat("Horizontal", 0);
-            animator.SetFloat("Vertical", 0);
+            animator.SetInteger("Horizontal", 0);
+            animator.SetInteger("Vertical", 0);
             animator.SetFloat("Speed", 0);
         }
     }
 
     void FixedUpdate()
     {
-        rBody.MovePosition(rBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (!activeLock)
+        {
+            rBody.MovePosition(rBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 
     public void LockMovement()
